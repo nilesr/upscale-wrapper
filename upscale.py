6333f47e702bc41637a4c0ad4d9575dad819047a
Niles Rogoff <me@niles.xyz>
2017-02-10 18:38:48 -0500

    Infer use of second argument

--- upscale.py
+++ upscale.py
@@ -5,8 +5,12 @@ null = open(os.devnull, "w")
 infile = sys.argv[1]
 outfile = ".".join(sys.argv[1].split(".")[:-1]) + "-waifu2x.png"
 noise_level = False
-if len(sys.argv) > 2:
-    outfile = sys.argv[2]
+if len(sys.argv) == 2:
+    if sys.argv[2] in ["0", "1", "2"]:
+        print("Assuming you don't want to write to a file named " + sys.argv[2])
+        noise_level = int(sys.argv[2])
+    else:
+        outfile = sys.argv[2]
 if len(sys.argv) > 3:
     noise_level = int(sys.argv[3])
 if not noise_level or noise_level == 0:
